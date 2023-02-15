cb2ded3a8d639cc02501a56428f980d12625fbb5
Andy Mensah <122259813+penfoldslaw@users.noreply.github.com>
2023-02-15 11:26:27 -0500

    Card Identifier

---
+++ Card Identifier/Card Identifier/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Card_Identifier
+{
+    partial class Card_Identifier
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Card_Identifier));
+            this.pictureboxace = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Eight = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Joker = new System.Windows.Forms.PictureBox();
+            this.pictureBox_king = new System.Windows.Forms.PictureBox();
+            this.pictureBoxtwo = new System.Windows.Forms.PictureBox();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Eight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Joker)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_king)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxtwo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureboxace
+            // 
+            this.pictureboxace.Image = ((System.Drawing.Image)(resources.GetObject("pictureboxace.Image")));
+            this.pictureboxace.Location = new System.Drawing.Point(27, 32);
+            this.pictureboxace.Name = "pictureboxace";
+            this.pictureboxace.Size = new System.Drawing.Size(102, 135);
+            this.pictureboxace.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureboxace.TabIndex = 0;
+            this.pictureboxace.TabStop = false;
+            this.pictureboxace.Click += new System.EventHandler(this.pictureboxace_Click);
+            // 
+            // pictureBox_Eight
+            // 
+            this.pictureBox_Eight.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Eight.Image")));
+            this.pictureBox_Eight.Location = new System.Drawing.Point(374, 32);
+            this.pictureBox_Eight.Name = "pictureBox_Eight";
+            this.pictureBox_Eight.Size = new System.Drawing.Size(106, 135);
+            this.pictureBox_Eight.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Eight.TabIndex = 1;
+            this.pictureBox_Eight.TabStop = false;
+            this.pictureBox_Eight.Click += new System.EventHandler(this.pictureBox_Eight_Click);
+            // 
+            // pictureBox_Joker
+            // 
+            this.pictureBox_Joker.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_Joker.Image")));
+            this.pictureBox_Joker.Location = new System.Drawing.Point(750, 32);
+            this.pictureBox_Joker.Name = "pictureBox_Joker";
+            this.pictureBox_Joker.Size = new System.Drawing.Size(100, 135);
+            this.pictureBox_Joker.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Joker.TabIndex = 2;
+            this.pictureBox_Joker.TabStop = false;
+            this.pictureBox_Joker.Click += new System.EventHandler(this.pictureBox_Joker_Click);
+            // 
+            // pictureBox_king
+            // 
+            this.pictureBox_king.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox_king.Image")));
+            this.pictureBox_king.Location = new System.Drawing.Point(554, 32);
+            this.pictureBox_king.Name = "pictureBox_king";
+            this.pictureBox_king.Size = new System.Drawing.Size(100, 135);
+            this.pictureBox_king.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_king.TabIndex = 3;
+            this.pictureBox_king.TabStop = false;
+            this.pictureBox_king.Click += new System.EventHandler(this.pictureBox_king_Click);
+            // 
+            // pictureBoxtwo
+            // 
+            this.pictureBoxtwo.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxtwo.Image")));
+            this.pictureBoxtwo.Location = new System.Drawing.Point(194, 32);
+            this.pictureBoxtwo.Name = "pictureBoxtwo";
+            this.pictureBoxtwo.Size = new System.Drawing.Size(100, 135);
+            this.pictureBoxtwo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxtwo.TabIndex = 4;
+            this.pictureBoxtwo.TabStop = false;
+            this.pictureBoxtwo.Click += new System.EventHandler(this.pictureBoxtwo_Click);
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelOutput.Location = new System.Drawing.Point(194, 224);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(460, 40);
+            this.labelOutput.TabIndex = 5;
+            this.labelOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelOutput.Click += new System.EventHandler(this.labelOutput_Click);
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Location = new System.Drawing.Point(27, 318);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(131, 43);
+            this.Clearbutton.TabIndex = 6;
+            this.Clearbutton.Text = "Clear Text";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            this.Clearbutton.Click += new System.EventHandler(this.Clearbutton_Click);
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Location = new System.Drawing.Point(717, 318);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(133, 43);
+            this.Exitbutton.TabIndex = 7;
+            this.Exitbutton.Text = "Exit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // Card_Identifier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 401);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.pictureBoxtwo);
+            this.Controls.Add(this.pictureBox_king);
+            this.Controls.Add(this.pictureBox_Joker);
+            this.Controls.Add(this.pictureBox_Eight);
+            this.Controls.Add(this.pictureboxace);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Card_Identifier";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Card_Identifier";
+            this.Load += new System.EventHandler(this.Card_Identifier_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureboxace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Eight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Joker)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_king)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxtwo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureboxace;
+        private System.Windows.Forms.PictureBox pictureBox_Eight;
+        private System.Windows.Forms.PictureBox pictureBox_Joker;
+        private System.Windows.Forms.PictureBox pictureBox_king;
+        private System.Windows.Forms.PictureBox pictureBoxtwo;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Button Exitbutton;
+    }
+}
+
---
+++ Card Identifier/Card Identifier/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Card_Identifier
+{
+    public partial class Card_Identifier : Form
+    {
+        public Card_Identifier()
+        {
+            InitializeComponent();
+        }
+
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureboxace_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Ace of Spades";
+        }
+
+        private void pictureBoxtwo_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = "Two of Clubs";
+        }
+        private void pictureBox_Eight_Click(object sender, EventArgs e)
+        {
+                labelOutput.Text = "Eight of Diamonds";
+        }
+
+        private void pictureBox_king_Click(object sender, EventArgs e)
+        {
+                labelOutput.Text = "King of Spades";
+        }
+
+        private void pictureBox_Joker_Click(object sender, EventArgs e)
+        {
+                labelOutput.Text = "Joker";
+        }
+
+        private void Clearbutton_Click(object sender, EventArgs e)
+        {
+            labelOutput.Text = String.Empty;
+        }
+
+        private void Card_Identifier_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
