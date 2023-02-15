5bb8f0c59400d82b492c130c292d9590c10a6d3d
Andy Mensah <122259813+penfoldslaw@users.noreply.github.com>
2023-02-15 11:33:29 -0500

    split bill

---
+++ Split Bill App/Split Bill App/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Split_Bill_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTotalbill = new System.Windows.Forms.TextBox();
+            this.textBoxNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCost = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Total Bill";
+            // 
+            // textBoxTotalbill
+            // 
+            this.textBoxTotalbill.Location = new System.Drawing.Point(283, 39);
+            this.textBoxTotalbill.Name = "textBoxTotalbill";
+            this.textBoxTotalbill.Size = new System.Drawing.Size(100, 35);
+            this.textBoxTotalbill.TabIndex = 0;
+            this.textBoxTotalbill.Text = "120";
+            this.textBoxTotalbill.TextChanged += new System.EventHandler(this.textBoxTotalbill_TextChanged);
+            // 
+            // textBoxNumber
+            // 
+            this.textBoxNumber.Location = new System.Drawing.Point(283, 132);
+            this.textBoxNumber.Name = "textBoxNumber";
+            this.textBoxNumber.Size = new System.Drawing.Size(100, 35);
+            this.textBoxNumber.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of People";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(195, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cost Per Person:";
+            // 
+            // labelCost
+            // 
+            this.labelCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCost.Location = new System.Drawing.Point(290, 235);
+            this.labelCost.Name = "labelCost";
+            this.labelCost.Size = new System.Drawing.Size(93, 20);
+            this.labelCost.TabIndex = 5;
+            this.labelCost.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelCost.Click += new System.EventHandler(this.labelCost_Click);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(442, 39);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(143, 42);
+            this.buttonCalculate.TabIndex = 6;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(442, 125);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(143, 42);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(442, 222);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(143, 42);
+            this.buttonExit.TabIndex = 8;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 314);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelCost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxTotalbill);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Split Bill App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTotalbill;
+        private System.Windows.Forms.TextBox textBoxNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCost;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Split Bill App/Split Bill App/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Split_Bill_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            labelCost.Text = string.Empty;
+            textBoxTotalbill.Text = string.Empty;
+            textBoxNumber.Text = string.Empty;
+            // another way of clearing text in a textbox
+            //textBoxNumber.Clear();
+            textBoxTotalbill.Focus();
+        }
+
+        private void labelCost_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            //input 
+            //declare variables
+            decimal inTotal = 0m;
+            int inNumber = 0;
+            decimal outCost = 0m;
+
+            //get inputs from the textboxes 
+            inTotal = decimal.Parse(textBoxTotalbill.Text);
+            inNumber = int.Parse(textBoxNumber.Text);
+
+            //process
+            outCost = inTotal/ inNumber;
+
+            //output 
+            labelCost.Text = outCost.ToString("c4");
+        }
+
+        private void textBoxTotalbill_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
